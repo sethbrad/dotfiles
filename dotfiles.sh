de851308df65a30e2f694816be8306c748e90b23
Seth Braddock <sethbrad99@gmail.com>
2022-05-17 10:35:54 -0500

    got the real dotfiles in here

---
+++ dotfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# A script to copy configuration files to a repository folder. In the future, this should also copy the backups to their original spots
+
+cp ~/.config/i3/config ~/dotfiles/i3.conf.bak
+cp ~/.config/polybar/config.ini ~/dotfiles/pb.conf.bak
+cp ~/.config/polybar/launch.sh ~/dotfiles/pb.launch.bak
+cp ~/.tmux.conf ~/dotfiles/tmux.conf.bak
+cp ~/.config/omf/init.fish ~/dotfiles/omf.conf.bak
+# add stuff for Nvim
